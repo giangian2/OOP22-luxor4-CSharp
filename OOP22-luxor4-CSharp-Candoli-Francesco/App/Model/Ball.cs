2aa276fc094fbce8900d0b6a760b80ef4241c625
Francesco Candoli <francesco.candoli@studio.unibo.it>
2023-08-23 14:23:33 +0200

    some changes

--- OOP22-luxor4-CSharp-Candoli-Francesco/App/Model/Ball.cs
+++ OOP22-luxor4-CSharp-Candoli-Francesco/App/Model/Ball.cs
@@ -6,58 +6,37 @@ namespace OOP22_luxor4_CSharp_Candoli_Francesco.App.Model
 {
     public class Ball : GameObject
     {
-        private BallColor _color;
-        public static readonly int IMAGE_DIAMETER = 25;
+        private BallColor Color {get; set;}
+        public const  int IMAGE_DIAMETER = 25;
 
-        /**
-        * Constructor for an istance of Ball.
-        * 
-        * @param type    Specified type of game object.
-        * @param pos     Position (coordinates)
-        * @param color   Color
-        * @param vel     Velocity vector
-        * @param input   Input component
-        * @param bbox    Bounding box
-        * @param physics Physics component
-        * @param graph   Graphic component
-        */
+        
+        /// <summary>
+        /// Constructor for an instance of Ball.
+        /// </summary>
+        /// <param name="type">Specified type of game object.</param>
+        /// <param name="pos">Position (coordinates).</param>
+        /// <param name="color">Color.</param>
+        /// <param name="vel">Velocity vector.</param>
+        /// <param name="input">Input component.</param>
+        /// <param name="bbox">Bounding box.</param>
+        /// <param name="physics">Physics component.</param>
+        /// <param name="graph">Graphic component.</param>
         public Ball(Type type, P2d pos, BallColor color,
                 V2d vel, InputComponent input, BoundingBox bbox,
-                IPhysicsComponent physics, BallGraphicsComponent graph) {
-
-            super(type, pos, vel, input, bbox, graph, physics);
-            this.color = color;
+                IPhysicsComponent physics, BallGraphicsComponent graph)
+                : base(type, pos, vel, input, bbox, graph, physics)
+        {
+            this.Color = color;
         }
 
-        /**
-        * Set the color of the ball.
-        * 
-        * @param color Color to set
-        * @see BallColor
-        */
-        public void setColor(final BallColor color) {
-            this.color = color;
-        }
-
-        /**
-        * Get the color of the ball.
-        * 
-        * @see BallColor
-        * @return The color of the ball
-        */
-        public BallColor getColor() {
-            return this.color;
-        }
-
-        /**
-        * Method that return if a ball(a) if the distance beetween a and this ball
-        * is less than its diameter + a little approximation).
-        * 
-        * @param a Ball to check if is near
-        * @return True if the ball is near else false
-        */
-        public boolean isNear(final Ball a) {
-            return Math.abs(this.getCurrentPos().sumOfAxis() - a.getCurrentPos().sumOfAxis()) < (IMAGE_DIAMETER + 2);
+        /// <summary>
+        /// Method that returns if a ball (a) if the distance between a and this ball
+        /// is less than its diameter + a little approximation).
+        /// </summary>
+        /// <param name="a">Ball to check if is near</param>
+        /// <returns>True if the ball is near else false</returns>
+        public bool isNear(Ball a) {
+            return Math.Abs(this.getCurrentPos().sumOfAxis() - a.getCurrentPos().sumOfAxis()) < (IMAGE_DIAMETER + 2);
         }
 
         /**
@@ -65,12 +44,9 @@ namespace OOP22_luxor4_CSharp_Candoli_Francesco.App.Model
         * 
         * @return Hashcode.
         */
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((color == null) ? 0 : color.hashCode());
-            return result;
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Color);
         }
 
         /**
@@ -78,23 +54,11 @@ namespace OOP22_luxor4_CSharp_Candoli_Francesco.App.Model
         * 
         * @return true if this Ball is equal to the argument, false otherwise.
         */
-        @Override
-        public boolean equals(final Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final Ball other = (Ball) obj;
-            if (color != other.color) {
-                return false;
-            }
-            return getCurrentPos().equals(other.getCurrentPos());
-
+        public bool Equals(Ball other)
+        {
+            return other != null &&
+                this.Color == other.Color &&
+                this.getCurrentPos() == other.getCurrentPos();
         }
 
     }
